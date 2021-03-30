963a9880a6208127045abad68624ab7d065305bb
unknown <vquivy@gamingcampus.local>
2021-03-30 15:08:39 +0200

    Rendu Exo 2

---
+++ Exo2.py
@@ -0,0 +1,38 @@
+import math
+
+#cour 2 exo 1
+
+'''entre = float(input("Votre chiffre ?"))
+if entre >= 0:
+    print(math.sqrt(entre))
+else:
+    print("Erreur ton chiffre est négatif")'''
+
+#cour 2 exo 2
+
+'''mot1 = input('premier mot ? ')
+mot2 = input('deuxieme mot ? ')
+if mot1 < mot2:
+    print(mot1)
+elif mot2< mot1:
+    print(mot2)
+else:
+    print('meme mot')
+
+res = mot1 if mot1 < mot2 else mot2 if mot2<mot1 else "meme mot"
+print(res)'''
+
+#cour 2 exo 3
+
+'''pSeuil = 2.3
+vSeuil = 7.41
+vol = int(input('Le volume ? '))
+pression = int(input('La pression ? '))
+if vol > vSeuil and pression > pSeuil:
+    print('Shutdown')
+if vol < vSeuil and pression > pSeuil:
+    print('Volume up')
+if vol > vSeuil and pression < pSeuil:
+    print('Volume Down')
+if vol < vSeuil and pression < pSeuil:
+    print('Fine')'''
